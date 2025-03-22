b3e9f387c844c60283697bae1ad47a18fe5ee65d
Indhu <indhursty@gmail.com>
2025-03-22 15:03:46 +0530

    Removed build folder from repo and added to .gitignore

--- build/lib/waveform_generator/__init__.py
+++
@@ -1,8 +0,0 @@
-#Package Initialization
-#makes waveform_generator a package
-
-from .generator import generate_waveform
-from .plotter import plot_waveform
-from .file_handler import save_to_csv
-
-__all__=["generate_waveform", "plot_waveform", "save_to_csv"]
